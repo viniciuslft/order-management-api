1a2145c80dfbe462f1bfa38113ba134168299b83
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-09 19:18:31 -0300

    OMA-53 | :lock: Validate item fields

--- src/middlewares/validateOrder.js
+++ src/middlewares/validateOrder.js
@@ -1,19 +1,16 @@
 const Joi = require('joi');
 
+const itemSchema = Joi.object({
+  idItem: Joi.string().trim().required(),
+  quantidadeItem: Joi.number().integer().greater(0).required(),
+  valorItem: Joi.number().greater(0).required()
+});
+
 const orderSchema = Joi.object({
-  numeroPedido: Joi.string().required(),
-  valorTotal: Joi.number().required(),
+  numeroPedido: Joi.string().trim().required(),
+  valorTotal: Joi.number().greater(0).required(),
   dataCriacao: Joi.string().required(),
-  items: Joi.array()
-    .items(
-      Joi.object({
-        idItem: Joi.string().required(),
-        quantidadeItem: Joi.number().required(),
-        valorItem: Joi.number().required()
-      })
-    )
-    .min(1)
-    .required()
+  items: Joi.array().items(itemSchema).min(1).required()
 });
 
 function validateOrder(req, res, next) {
